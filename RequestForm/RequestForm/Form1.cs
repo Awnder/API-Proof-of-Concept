12831206e2ae6fb8f74269962367ae403aa6ce06
Andrew Shiroma <amshiroma@zoho.com>
2022-10-17 21:33:01 -0700

    Created first form with opening comments

---
+++ RequestForm/RequestForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿///Purpose/title of app, requesting something
+///
+///Created by Viktor Berg, Brian Ly, Masaya Takahashi, and Andrew Shiroma 
+///Time due at October 24th
+///
+///Github link: https://github.com/Awnder/API-Proof-of-Concept
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RequestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
